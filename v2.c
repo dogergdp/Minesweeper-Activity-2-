fe4b9dc32633d2d1080b708495a69e7bceeb21ad
dogergdp <118601243+dogergdp@users.noreply.github.com>
2023-04-30 16:18:20 +0800

    Add files via upload

--- v2.c
+++ v2.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <windows.h>
 
 #define ROWS 10
 #define COLS 10
-#define MINES 15
-#define LIVES 3
+#define NUM_MINES 5
 
 int board[ROWS][COLS];
 int revealed[ROWS][COLS];
-int mines[ROWS][COLS];
-int mines_left = MINES;
-int bomb_locations[MINES][2];
-struct move {
-    int x;
-    int y;
-    int is_bomb;
-};
+int lives = 3;
+int mines_left = NUM_MINES;
 
-struct move moves[ROWS * COLS];
-int num_moves = 0;
+
+void print_line(int LENGTH){
+	int i;
+	for (i=0; i<LENGTH; i++){
+		printf("%c", 205);
+	}
+}
+
 
 void init_board() {
     int i, j, k;
@@ -29,15 +29,13 @@ void init_board() {
             revealed[i][j] = 0;
         }
     }
-    for (k = 0; k < MINES; k++) {
+    for (k = 0; k < NUM_MINES; k++) {
         int i, j;
         do {
             i = rand() % ROWS;
             j = rand() % COLS;
         } while (board[i][j] == -1);
         board[i][j] = -1;
-        bomb_locations[k][0] = i;
-        bomb_locations[k][1] = j;
         for (int di = -1; di <= 1; di++) {
             for (int dj = -1; dj <= 1; dj++) {
                 int ni = i + di;
@@ -50,36 +48,61 @@ void init_board() {
     }
 }
 
-
 void print_board() {
     int i, j;
-    printf("Mines left: %d\n", mines_left);
-    printf("  ");
-    for (j = 0; j < COLS; j++) {
-        printf("%d ", j);
-    }
     printf("\n");
-    printf("  ");
-    for (j = 0; j < COLS; j++) {
-        printf("--");
-    }
-    printf("\n");
+    printf("\t\t\t\t\t\t+");print_line(16);printf("+");print_line(13);printf("+");printf("\n");
+    printf("\t\t\t\t\t\t| Mines left: %d  | %c  Lives: %d |\n", mines_left, 3, lives);
+    printf("\t\t\t\t\t\t+");print_line(16);printf("+");print_line(13);printf("+");printf("\n");
+
+    
+    printf("\t\t\t\t\t");printf("+");print_line(3);printf("+");print_line(39);printf("+");printf("\n");
+    printf("\t\t\t\t\t|   | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |\n");
+    printf("\t\t\t\t\t");printf("+");print_line(3);printf("+");print_line(39);printf("+");printf("\n");
+    
     for (i = 0; i < ROWS; i++) {
-        printf("%d|", i);
+        printf("\t\t\t\t\t| %d ", i);
         for (j = 0; j < COLS; j++) {
             if (revealed[i][j]) {
                 if (board[i][j] == -1) {
-                    printf("* ");
+                    printf("| %c ", 149);
                 } else {
-                    printf("%d ", board[i][j]);
+                    switch (board[i][j]) {
+                        case 1:
+                            printf("| \033[1;34m%d\033[0m ", board[i][j]); // blue
+                            break;
+                        case 2:
+                            printf("| \033[1;32m%d\033[0m ", board[i][j]); // green
+                            break;
+                        case 3:
+                            printf("| \033[1;31m%d\033[0m ", board[i][j]); // red
+                            break;
+                        case 4:
+                            printf("| \033[1;35m%d\033[0m ", board[i][j]); // magenta
+                            break;
+                        case 5:
+                            printf("| \033[1;33m%d\033[0m ", board[i][j]); // yellow
+                            break;
+                        case 6:
+                        case 7:
+                        case 8:
+                            printf("| \033[1;36m%d\033[0m ", board[i][j]); // cyan
+                            break;
+                        default:
+                            printf("| \033[1m%d\033[0m ", board[i][j]); // bold
+                            break;
+                    }
                 }
             } else {
-                printf(". ");
+                printf("| %c ", 219);
             }
         }
-        printf("\n");
+        printf("|\n");
+        printf("\t\t\t\t\t");printf("+");print_line(3);printf("+");print_line(39);printf("+");printf("\n");
     }
 }
+
+
 
 
 void reveal(int i, int j) {
@@ -88,13 +111,13 @@ void reveal(int i, int j) {
     }
     revealed[i][j] = 1;
     if (board[i][j] == -1) {
-        printf("Game over!\n");
-        moves[num_moves].x = i;
-        moves[num_moves].y = j;
-        moves[num_moves].is_bomb = 1;
-        num_moves++;
-        print_board();
-        exit(0);
+        printf("Mine hit!\n");
+        lives--;
+        mines_left--;
+        if (lives == 0) {
+            printf("Game over!\n");
+            exit(0);
+        }
     } else if (board[i][j] == 0) {
         for (int di = -1; di <= 1; di++) {
             for (int dj = -1; dj <= 1; dj++) {
@@ -105,29 +128,20 @@ void reveal(int i, int j) {
                 }
             }
         }
-    } else {
-        moves[num_moves].x = i;
-        moves[num_moves].y = j;
-        moves[num_moves].is_bomb = 0;
-        num_moves++;
     }
-    print_board();
-    fflush(stdout);
 }
-
-
-
-
-
 
 int main() {
     srand(time(NULL));
     init_board();
-    int lives = LIVES;
+    int lives = 3;
+    int non_bombs_left = ROWS * COLS - NUM_MINES;
     while (1) {
-        printf("\nLives left: %d\n", lives);
+        system("cls");
         int x, y;
-        printf("Enter x and y coordinates (separated by a space): ");
+        print_board();
+        printf("\n\n");
+        printf("\t\t\t\t  Enter x and y coordinates (separated by a space): ");
         scanf("%d %d", &x, &y);
         if (x < 0 || x >= ROWS || y < 0 || y >= COLS) {
             printf("Invalid coordinates. Try again.\n");
@@ -137,21 +151,22 @@ int main() {
             printf("This tile has already been revealed. Try again.\n");
             continue;
         }
+        reveal(x, y);
         if (board[x][y] == -1) {
-            printf("Game over!\n");
-            reveal(x, y); // show the bomb location
-            moves[num_moves].x = x;
-            moves[num_moves].y = y;
-            moves[num_moves].is_bomb = 1;
-            num_moves++;
-            exit(0);
-        } else {
-            reveal(x, y);
-            if (--mines_left == 0) {
-                printf("You win!\n");
+            lives--;
+            printf("You hit a mine! You have %d lives left.\n", lives);
+            if (lives == 0) {
+                printf("Game over!\n");
                 exit(0);
             }
+        } else {
+            non_bombs_left--;
+        }
+        if (non_bombs_left == 0) {
+            printf("You win!\n");
+            exit(0);
         }
     }
     return 0;
 }
+
